d9edd91cdbcbf7d51aca4e1510ca306f0025b3a5
yash-git-max <yashwanthbashipally2005@gmail.com>
2026-06-05 13:09:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
- 
+
 const PORT = 3000;
- 
+
 app.get('/', (req, res) => {
-  res.send('Hello from DevOps Freshers);
+  res.send('POC-22 CI/CD Working Successfully ');
 });
- 
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
